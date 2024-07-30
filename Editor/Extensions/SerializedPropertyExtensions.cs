dd4c545ca8317591293f996e138c4390f57cff25
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2024-07-30 12:27:22 -0500

    Fixed 2 issues with SerializedPropertyExtensions

--- Editor/Extensions/SerializedPropertyExtensions.cs
+++ Editor/Extensions/SerializedPropertyExtensions.cs
@@ -15,6 +15,7 @@ namespace NAK.LuaTools.Extensions
             property.arraySize++;
             SerializedProperty newArrayElement = property.GetArrayElementAtIndex(property.arraySize - 1);
             newArrayElement.SetDefaultValues(typeof(T));
+            newArrayElement.isExpanded = false;
             return newArrayElement;
         }
 
@@ -31,6 +32,7 @@ namespace NAK.LuaTools.Extensions
 
                 const int DefaultArraySize = 0; // TODO: figure out how to determine this
                 property.arraySize = DefaultArraySize;
+                property.isExpanded = false;
                 for (int i = 0; i < property.arraySize; i++)
                 {
                     SerializedProperty elementProperty = property.GetArrayElementAtIndex(i);
@@ -44,8 +46,8 @@ namespace NAK.LuaTools.Extensions
                 Debug.LogWarning($"Type {type.Name} does not have a default constructor");
                 return;
             }
-            
-            object instance = Activator.CreateInstance(type);
+
+            object instance = !type.IsSubclassOf(typeof(ScriptableObject)) ? Activator.CreateInstance(type) : null;
 
             foreach (FieldInfo fieldInfo in type.GetFields(BindingFlags.Public | BindingFlags.Instance))
             {
@@ -80,99 +82,95 @@ namespace NAK.LuaTools.Extensions
             //     if (fieldType.IsClass) fieldProperty.SetDefaultValues(fieldType);
             //     return;
             // }
-
-            if (value == null) value = default;
-
-            // TODO: double check this supports all types that SerializedProperty supports (gui-wise) by default
             
             if (fieldType == typeof(int))
             {
-                fieldProperty.intValue = (int)value;
+                fieldProperty.intValue = value as int? ?? default;
             }
             else if (fieldType == typeof(bool))
             {
-                fieldProperty.boolValue = (bool)value;
+                fieldProperty.boolValue = value as bool? ?? default;
             }
             else if (fieldType == typeof(float))
             {
-                fieldProperty.floatValue = (float)value;
+                fieldProperty.floatValue = value as float? ?? default;
             }
             else if (fieldType == typeof(string))
             {
-                fieldProperty.stringValue = (string)value;
+                fieldProperty.stringValue = value as string ?? string.Empty;
             }
             else if (fieldType == typeof(Color))
             {
-                fieldProperty.colorValue = (Color)value;
+                fieldProperty.colorValue = value as Color? ?? default;
             }
-            else if (fieldType == typeof(Object))
+            else if (fieldType == typeof(Object) || fieldType.IsSubclassOf(typeof(Object)))
             {
-                fieldProperty.objectReferenceValue = (Object)value;
+                fieldProperty.objectReferenceValue = value as Object;
             }
             else if (fieldType == typeof(LayerMask))
             {
-                fieldProperty.intValue = (value is LayerMask mask ? mask : default).value;
+                fieldProperty.intValue = value is LayerMask mask ? mask.value : default;
             }
             else if (fieldType.IsEnum)
             {
-                fieldProperty.enumValueIndex = (int)value; // todo: verify
-                fieldProperty.intValue = (int)value;
+                fieldProperty.enumValueIndex = value as int? ?? default; // Verify if correct
+                fieldProperty.intValue = value as int? ?? default;
             }
             else if (fieldType == typeof(Vector2))
             {
-                fieldProperty.vector2Value = (Vector2)value;
+                fieldProperty.vector2Value = value as Vector2? ?? default;
             }
             else if (fieldType == typeof(Vector3))
             {
-                fieldProperty.vector3Value = (Vector3)value;
+                fieldProperty.vector3Value = value as Vector3? ?? default;
             }
             else if (fieldType == typeof(Vector4))
             {
-                fieldProperty.vector4Value = (Vector4)value;
+                fieldProperty.vector4Value = value as Vector4? ?? default;
             }
             else if (fieldType == typeof(Rect))
             {
-                fieldProperty.rectValue = (Rect)value;
+                fieldProperty.rectValue = value as Rect? ?? default;
             }
             else if (fieldType == typeof(char))
             {
-                fieldProperty.intValue = (int)value;
+                fieldProperty.intValue = value as char? ?? default;
             }
             else if (fieldType == typeof(AnimationCurve))
             {
-                fieldProperty.animationCurveValue = (AnimationCurve)value;
+                fieldProperty.animationCurveValue = value as AnimationCurve;
             }
             else if (fieldType == typeof(Bounds))
             {
-                fieldProperty.boundsValue = (Bounds)value;
+                fieldProperty.boundsValue = value as Bounds? ?? default;
             }
             else if (fieldType == typeof(Gradient))
             {
-                //fieldProperty.gradientValue = (Gradient)value; // is internal?
+                // fieldProperty.gradientValue = value as Gradient; // internal unity property
+            }
+            else if (fieldType == typeof(Vector2Int))
+            {
+                fieldProperty.vector2IntValue = value as Vector2Int? ?? default;
+            }
+            else if (fieldType == typeof(Vector3Int))
+            {
+                fieldProperty.vector3IntValue = value as Vector3Int? ?? default;
+            }
+            else if (fieldType == typeof(RectInt))
+            {
+                fieldProperty.rectIntValue = value as RectInt? ?? default;
+            }
+            else if (fieldType == typeof(BoundsInt))
+            {
+                fieldProperty.boundsIntValue = value as BoundsInt? ?? default;
+            }
+            else if (fieldType == typeof(Quaternion))
+            {
+                fieldProperty.quaternionValue = value as Quaternion? ?? default;
             }
             else if (fieldType.IsClass)
             {
                 fieldProperty.SetDefaultValues(fieldType);
-            }
-            else if (fieldType == typeof(Vector2Int))
-            {
-                fieldProperty.vector2IntValue = (Vector2Int)value;
-            }
-            else if (fieldType == typeof(Vector3Int))
-            {
-                fieldProperty.vector3IntValue = (Vector3Int)value;
-            }
-            else if (fieldType == typeof(RectInt))
-            {
-                fieldProperty.rectIntValue = (RectInt)value;
-            }
-            else if (fieldType == typeof(BoundsInt))
-            {
-                fieldProperty.boundsIntValue = (BoundsInt)value;
-            }
-            else if (fieldType == typeof(Quaternion))
-            {
-                fieldProperty.quaternionValue = (Quaternion)value;
             }
             else
             {
